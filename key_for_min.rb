de4245ae08d59371a4ef2fcd44ee27cc308173f8
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-29 23:43:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,33 @@
+  require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+# binding.pry
+    if name_hash.invert.size {|key, value| value < name_hash }
+# binding.pry
+      puts name_hash
+        elsif
+# binding.pry
+      puts
+    end
+    nil
 end
+
+# def key_for_min_value(name_hash)
+#               # binding.pry
+#   if name_hash.invert.size {|key, value| value < name_hash }
+#               # binding.pry
+#               puts name_hash
+#   elsif name_hash == {}
+#               # binding.pry
+#       puts nil
+#   else name_hash == {}
+#       puts nil
+#     end
+#   name_hash
+# end
+
+# elsif name_hash1 - name_hash2 <= 0
+
+#   puts name_hash1
