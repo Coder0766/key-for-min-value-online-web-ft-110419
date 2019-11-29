fe78f174576798d4e7136beae67d0d476de7c4d8
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-29 23:55:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -75,7 +75,7 @@ key= 0
 # Iterate new array
 name_arr.each do |i|
 # If current value is lower than default, change value&key
-  if i[1] < value 
+  if i[1] < value
     value = i[1]
       key = i[0]
         else name_hash = name_hash{}
