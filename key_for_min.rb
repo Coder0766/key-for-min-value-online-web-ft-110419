8af73e339c70307e1bb7df04bb60fd746a7d3586
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-30 01:10:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -67,8 +67,8 @@
 def key_for_min_value(name_hash)
 name_arr = name_hash.to_a
 a_key= 0
-a_value >= 1
-name_arr.each do |i|
+a_value= 1000
+name_arr.each.collect do |i|
   if i[1] < a_value
     a_key = i[0]
       a_value = i[1]
